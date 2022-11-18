dd34de4dfc05d8a9dcda033ee031a55321459162
EC2 Default User <ec2-user@ip-172-31-22-26.ap-northeast-1.compute.internal>
2022-11-18 10:45:31 +0000

    [Update]課題３　

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,37 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new 
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
+    # books#showにリダイレクトしないといけない
+    redirect_to user_path(current_user.id)
   end
 
   def index
-    
+    @new_book = Book.new
+    @books = Book.all
+    @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
   end
-  
-  
+
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+  def user_params
+    # ここに、画像も許可する記述が必要
+    params.require(:user).permit(:name,:introduction,:profile_image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to:"homes#top"
   devise_for :users
-  
+
   get '/homes/about' => 'homes#about', as: 'about'
 
-  resources :books,only: [:new,:index,:show]
+  resources :books,only: [:new,:create,:index,:show]
   resources :users,only: [:show,:index,:edit]
+  patch 'users/:id' => 'users#update', as: 'update_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
