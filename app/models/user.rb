bd561f290918a47066494d287d8e29e89ed9689c
EC2 Default User <ec2-user@ip-172-31-22-26.ap-northeast-1.compute.internal>
2022-12-05 08:08:22 +0000

    [finish]応用課題５

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,29 +7,43 @@ class User < ApplicationRecord
          has_many :books,dependent: :destroy
          has_many :favorites,dependent: :destroy
          has_many :book_comments, dependent: :destroy
-         
+
         # フォローをした、されたの関係
         has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
         has_many :followings, through: :relationships, source: :followed  #自分がフォローしている人の一覧表示のための記述
-        
+
         has_many :reverce_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
         has_many :followers, through: :reverce_of_relationships, source: :follower #自分がフォローされている人の一覧表示のための記述
-        
+
         # ユーザーをフォローする定義
         def follow(user_id)
           relationships.create(followed_id: user_id)
-        end 
-        
+        end
+
         # ユーザのフォローを外す定義
         def unfollow(user_id)
           relationships.find_by(followed_id: user_id).destroy
-        end 
-        
+        end
+
         # ユーザをフォローしているかの判定
         def following?(user)
           followings.include?(user)
         end
-        
+
+        # user検索方法分岐
+        def self.looks(search, word)
+          if search == "perfect_match"
+            @user = User.where("name LIKE?", "#{word}")
+          elsif search == "forward_match"
+            @user = User.where("name LIKE?", "#{word}%")
+          elsif search == "backward_match"
+            @user = User.where("name LIKE?", "%#{word}")
+          elsif search == "partial_match"
+            @user = User.where("name LIKE?", "%#{word}%")
+          else
+            @user = User.all
+          end
+        end
 
   has_one_attached :profile_image
   # バリデーション
