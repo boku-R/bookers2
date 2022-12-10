8e6b362e8ac5372e2e560ed3cf17d4f3a8c1cefb
EC2 Default User <ec2-user@ip-172-31-22-26.ap-northeast-1.compute.internal>
2022-12-10 07:41:59 +0000

    [Finish]応用課題8d

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -42,8 +42,18 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    @books = Book.all
+    # @books = Book.all
     @user = current_user
+
+    if params[:latest]
+      @books = Book.latest
+    elsif params[:old]
+      @books = Book.old
+    elsif params[:star_count]
+      @books = Book.star_count
+    else
+      @books = Book.all
+    end
   end
 
   def show
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,22 +7,24 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import jQuery from "jquery"
 
-import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application"
-import '@fortawesome/fontawesome-free/js/all'
 
-//ratyの導入
+import '@fortawesome/fontawesome-free/js/all';
+import "../stylesheets/application"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+global.$ = jQuery;
+window.$ = jQuery;
+
 import Raty from "raty.js"
 window.raty = function(elem,opt) {
   let raty =  new Raty(elem,opt)
   raty.init();
   return raty;
 }
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
